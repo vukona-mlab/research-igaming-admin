33552061a63f1490526adeb81fd3704410d62594
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-19 16:01:20 +0200

    updated ListHeader styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/signin/SignIn";
 //import AdminDashboard from "./AdminDashboard";
-
+import ListHeader from "./components/common/ListHeader/ListHeader";
 const App = () => {
+  const [tab, setTab] = useState("");
+  const [listName, setListName] = useState("Clients");
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <ListHeader
+        listName={listName}
+        handleTabChange={setTab}
+        handleListNameChange={setListName}
+      />
+    </div>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<LoginPage />} />
+    //   </Routes>
+    // </Router>
   );
 };
 

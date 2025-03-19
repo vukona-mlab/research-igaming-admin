6f2d3ad7adea821f17de4622650aac139017a7c1
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-19 16:02:42 +0200

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/signin/SignIn";
 //import AdminDashboard from "./AdminDashboard";
-import ListHeader from "./components/common/ListHeader/ListHeader";
-const App = () => {
-  const [tab, setTab] = useState("");
-  const [listName, setListName] = useState("Clients");
 
+const App = () => {
   return (
-    <div>
-      <ListHeader
-        listName={listName}
-        handleTabChange={setTab}
-        handleListNameChange={setListName}
-      />
-    </div>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<LoginPage />} />
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 };
 

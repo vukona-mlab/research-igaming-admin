321cdc6d75e53d604438529e81a245c7c6136a07
MabohlaleNkuna <111710433+MabohlaleNkuna@users.noreply.github.com>
2025-03-19 15:14:08 +0200

    added tabs header

---
+++ src/components/HeaderTabs/HeaderTabs.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './HeaderTabs.model.css'
+
+
+  const HeaderTabs = ({ tabOne, tabTwo, tabThree, handleTabChange }) => {
+    const [currentTab, setCurrentTab] = useState(tabOne);
+  
+    return (
+      <div className={styles.documentsHeader}>
+        <div
+          className={
+            currentTab === tabOne
+              ? `${styles.tbhTab} ${styles.currentTab}`
+              : styles.tbhTab
+          }
+          onClick={() => {
+            setCurrentTab(tabOne);
+            handleTabChange(tabOne);
+          }}
+        >
+          {tabOne}
+        </div>
+  
+        <div
+          className={
+            currentTab === tabTwo
+              ? `${styles.tbhTab} ${styles.currentTab}`
+              : styles.tbhTab
+          }
+          onClick={() => {
+            setCurrentTab(tabTwo);
+            handleTabChange(tabTwo);
+          }}
+        >
+          {tabTwo}
+        </div>
+  
+        <div
+          className={
+            currentTab === tabThree
+              ? `${styles.tbhTab} ${styles.currentTab}`
+              : styles.tbhTab
+          }
+          onClick={() => {
+            setCurrentTab(tabThree);
+            handleTabChange(tabThree);
+          }}
+        >
+          {tabThree}
+        </div>
+      </div>
+    );
+  };
+
+export default HeaderTabs;

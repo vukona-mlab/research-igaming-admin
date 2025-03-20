11acfc2c97fe07f5132738514a69cdac75d6819f
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2025-03-20 10:29:40 +0200

    Created table to render documentes uploaded.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import LoginPage from "./pages/signin/SignIn";
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return <div className="App">
+  </div>;
 };
 
 export default App;

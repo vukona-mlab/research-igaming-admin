3d056be7b8828bee76522df238157137992a1be3
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-20 09:58:53 +0200

    addded documents route

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/signin/SignIn";
 //import AdminDashboard from "./AdminDashboard";
 import FreelancerList from "./components/FreelancerList/FreelancerList";
-
+import DocumentsPage from "./pages/Documents/DocumentsPage";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/freelancer" element={<FreelancerList />} />
+        <Route path="/documents" element={<DocumentsPage />} />
       </Routes>
     </Router>
   );

70d14476e31c4164f0837d83f5cd0323819df0e7
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-24 13:58:45 +0200

    resolved conflicts - app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/signin/SignIn";
+//import AdminDashboard from "./AdminDashboard";
 import FreelancerList from "./components/FreelancerList/FreelancerList";
 import DocumentsPage from "./pages/Documents/DocumentsPage";
 import ProtectedRoutes from "./components/protected/ProtectedRoutes";

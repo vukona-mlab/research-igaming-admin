ae12800101bc52101d7ae3a16b52605a174c3880
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-25 09:49:22 +0200

    updated ProtectedAuth

--- src/components/protected/ProtectedAuth.jsx
+++ src/components/protected/ProtectedAuth.jsx
@@ -1,7 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function ProtectedAuth({ auth }) {
-  const user = localStorage.getItem("user");
+  const token = localStorage.getItem("authToken");
 
-  return user !== null && user !== "" ? <Navigate to="/" /> : <Outlet />;
+  return token !== null && token !== "" ? (
+    <Navigate to="/profile" />
+  ) : (
+    <Outlet />
+  );
 }

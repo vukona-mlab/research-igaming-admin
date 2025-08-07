719cda6ab1289c0445b80e330772160703761f2a
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-08-07 17:50:48 +0200

    updates

--- src/components/DocumentHeader/DocumentHeader.jsx
+++ src/components/DocumentHeader/DocumentHeader.jsx
@@ -20,19 +20,21 @@ const Documents = ({ searchTerm }) => {
       />
 
       {/* Buttons Section */}
-      {selectedTab !== "Notifications" && selectedTab !== "Account" && selectedTab !== "Reviews" && (
-        <div className="filter-buttons">
-          {["All", "Pending", "Approved", "Declined"].map((filter) => (
-            <button
-              key={filter}
-              className={selectedFilter === filter ? "selected" : ""}
-              onClick={() => setSelectedFilter(filter)}
-            >
-              {filter}
-            </button>
-          ))}
-        </div>
-      )}
+      {selectedTab !== "Notifications" &&
+        selectedTab !== "Account" &&
+        selectedTab !== "Reviews" && (
+          <div className="filter-buttons">
+            {["All", "Pending", "Approved", "Declined"].map((filter) => (
+              <button
+                key={filter}
+                className={selectedFilter === filter ? "selected" : ""}
+                onClick={() => setSelectedFilter(filter)}
+              >
+                {filter}
+              </button>
+            ))}
+          </div>
+        )}
 
       <div className="tab-content">
         {selectedTab === "Notifications" && (
--- src/components/DocumentsList/DocumentsList.jsx
+++ src/components/DocumentsList/DocumentsList.jsx
@@ -1,15 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect, act } from "react";
 import "./documentsList.css";
-import axios from "axios";
+import axios, { all } from "axios";
 import { FaEllipsisV } from "react-icons/fa";
 
 export default function DocumentsList() {
   const [loading, setLoading] = useState(false);
   const [documents, setDocuments] = useState([]);
   const [error, setError] = useState(null);
-
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [showContextMenu, setShowContextMenu] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [currentDocId, setCurrentDocId] = useState("");
+  const contextMenuRef = useRef(null);
   useEffect(() => {
     getDocuments();
+  }, []);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        contextMenuRef.current &&
+        !contextMenuRef.current.contains(event.target)
+      ) {
+        setShowContextMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
   const getDocuments = async () => {
@@ -31,9 +48,21 @@ export default function DocumentsList() {
           },
         }
       );
-      console.log("Fetched documents:", response.data.usersDocuments);
+      const arr = response.data.usersDocuments.map((user) => {
+        let docs = user.documents.filter((doc) => doc.id);
 
-      const allDocuments = response.data.usersDocuments.flat();
+        if (docs && docs.length > 0) {
+          docs = docs.map((doc) => {
+            return { ...doc, userId: user.id, email: user.email };
+          });
+          return docs;
+        }
+        return [];
+      });
+      console.log("Fetched documents:", arr.flat());
+
+      const allDocuments = arr.flat();
+
       setDocuments(allDocuments);
     } catch (error) {
       console.error("Error fetching documents:", error);
@@ -44,6 +73,45 @@ export default function DocumentsList() {
       setError(error.message);
     } finally {
       setLoading(false);
+    }
+  };
+
+  const handleContextMenuClick = (e) => {
+    e.stopPropagation();
+
+    setShowContextMenu(!showContextMenu);
+  };
+  const handleContextMenuAction = async (action, docId, userId) => {
+    try {
+      setIsUpdating(true);
+      const token = localStorage.getItem("authToken"); // Get the token which already includes 'Bearer' prefix
+      const status = action === "approve" ? "approved" : "declined";
+      console.log({
+        userId,
+        docId: docId,
+        status,
+      });
+      await axios.put(
+        `${import.meta.env.VITE_API_URL}/api/documents/${userId}/update-status`,
+        {
+          docId: docId,
+          status,
+        },
+        {
+          headers: {
+            Authorization: token, // Use the token directly as it already includes 'Bearer'
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      getDocuments();
+      // Call the parent component's callback to update the UI
+      setShowContextMenu(false);
+    } catch (error) {
+      console.error("Error updating document status:", error);
+      // You might want to show an error message to the user here
+    } finally {
+      setIsUpdating(false);
     }
   };
 
@@ -62,17 +130,59 @@ export default function DocumentsList() {
           </tr>
           {documents.map((document) => (
             <tr key={document.id}>
-              <td className="t-data">{document.name || "N/A"}</td>
+              <td className="t-data">{document.documentName || "N/A"}</td>
               <td className="t-data">{document.status || "N/A"}</td>
-              <td className="t-data">{document.type || "N/A"}</td>
+              <td className="t-data">{document.documentType || "N/A"}</td>
               <td className="t-data">{document.email || "N/A"}</td>
-              <td className="t-data">{document.date || "N/A"}</td>
-              <td className="t-data">{document.document || "N/A"}</td>
+              <td className="t-data">{document.dateAdded || "N/A"}</td>
+              <td className="t-data">{document.documentName || "N/A"}</td>
               <td className="t-data">
                 <div className="action-buttons">
-                  <div className="approved">{document.actions}Approved</div>
-                  <div className="dotted-menu">
-                    <FaEllipsisV />
+                  {/* <div className="approved">{document.actions}Approved</div> */}
+                  <div className="context-menu-container">
+                    <div
+                      className="dotted-menu"
+                      disabled={isUpdating || document.status == "approved"}
+                      onClick={(e) => {
+                        if (document.status != "approved") {
+                          setCurrentDocId(document.id);
+                          handleContextMenuClick(e);
+                        }
+                      }}
+                      aria-label="More options"
+                    >
+                      <FaEllipsisV />
+                    </div>
+                    {showContextMenu && currentDocId == document.id && (
+                      <div className="context-menu">
+                        <button
+                          className="context-menu-item approve"
+                          onClick={() =>
+                            handleContextMenuAction(
+                              "approve",
+                              document.id,
+                              document.userId
+                            )
+                          }
+                          disabled={isUpdating}
+                        >
+                          {isUpdating ? "Updating..." : "Approve"}
+                        </button>
+                        <button
+                          className="context-menu-item decline"
+                          onClick={() =>
+                            handleContextMenuAction(
+                              "decline",
+                              document.id,
+                              document.userId
+                            )
+                          }
+                          disabled={isUpdating}
+                        >
+                          {isUpdating ? "Updating..." : "Decline"}
+                        </button>
+                      </div>
+                    )}
                   </div>
                 </div>
               </td>
